6b77c076d80729a433f3cd1a1def8a5e32d1aa07
anajullia <anajullia@anajullia.com>
2023-05-01 16:35:39 -0300

    Versão 0.1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function corstevenon(){
+    $('.quadradoe').css({
+        'background-color': '#5c2c49',
+        'transform': 'scale(1.2)' // aumenta o tamanho em 20%
+    });
+}
+
+function corstevenout(){
+    $('.quadradoe').css({
+        'background-color': '#c86ca3', // define a cor de fundo original
+        'transform': 'scale(1.0)' // redefine o tamanho para o normal
+    });
+}
+
+function coraton() {
+    $('.quadradod').css({
+        'background-color': '#8b8a23',
+        'transform': 'scale(1.2)' // aumenta o tamanho em 20%
+    });
+}
+
+function coratout() {
+    $('.quadradod').css({
+        'background-color': '#dad72e',
+        'transform': 'scale(1.0)' // aumenta o tamanho em 20%
+    });
+}
+  
